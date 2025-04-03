27739fe20b1eefb82e3e8e91220ab74807e4939c
Niamh <niamh.linguist@gmail.com>
2025-04-03 17:08:57 +0300

    enemy follows player when player is in range

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -7,29 +7,27 @@ const JUMP_VELOCITY = 4.5
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 
 var player: CharacterBody3D
+var provoked := false
+var aggro_range := 12.0 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 
 func _process(_delta: float) -> void:
-	navigation_agent.target_position = player.global_position
+	if provoked:
+		navigation_agent.target_position = player.global_position
 	
 func _physics_process(delta: float) -> void:
 	var next_pos = navigation_agent.get_next_path_position()
 	
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = global_position.direction_to(next_pos)
+	var distance = global_position.distance_to(player.global_position)
+	provoked = distance <= aggro_range
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -37,4 +35,4 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	#move_and_slide()
+	move_and_slide()
