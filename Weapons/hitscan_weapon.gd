f3e510f35a4f846bf01e8eea562b03ddd81129cf
Niamh <niamh.linguist@gmail.com>
2025-04-04 12:17:46 +0300

    weapon recoil

---
+++ Weapons/hitscan_weapon.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@export var fire_rate := 14.0
+@export var recoil := 0.05
+@export var weapon_mesh: Node3D
+
+@onready var cooldown_timer: Timer = $CooldownTimer
+@onready var weapon_pos: Vector3 = weapon_mesh.position
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("fire"):
+		if cooldown_timer.is_stopped():
+			shoot()
+	
+	weapon_mesh.position = weapon_mesh.position.lerp(weapon_pos, delta * 10.0)
+
+func shoot() -> void:
+			cooldown_timer.start(1.0 / fire_rate)
+			print("weapon fired!")
+			weapon_mesh.position.z += recoil
+	
