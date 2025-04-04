77e1ea44af55405b13d080b84074bcd136463cc8
Niamh <niamh.linguist@gmail.com>
2025-04-04 17:26:58 +0300

    muzzleflash

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var recoil := 0.05
 @export var weapon_mesh: Node3D
 @export var weapon_damage := 15
+@export var muffle_flash: GPUParticles3D
 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var weapon_pos: Vector3 = weapon_mesh.position
@@ -17,10 +18,11 @@ func _process(delta: float) -> void:
 	weapon_mesh.position = weapon_mesh.position.lerp(weapon_pos, delta * 10.0)
 
 func shoot() -> void:
-			cooldown_timer.start(1.0 / fire_rate)
-			var collider = ray_cast.get_collider()
-			printt("weapon fired!", collider)
-			weapon_mesh.position.z += recoil
-			if collider is Enemy:
-				collider.hitpoints -= weapon_damage
+	muffle_flash.restart()
+	cooldown_timer.start(1.0 / fire_rate)
+	var collider = ray_cast.get_collider()
+	printt("weapon fired!", collider)
+	weapon_mesh.position.z += recoil
+	if collider is Enemy:
+		collider.hitpoints -= weapon_damage
 	
