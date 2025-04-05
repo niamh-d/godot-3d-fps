3450b64303b691f90d345de42e4034e4cb29106e
Niamh <niamh.linguist@gmail.com>
2025-04-05 15:33:29 +0300

    rifle

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -6,15 +6,21 @@ extends Node3D
 @export var weapon_damage := 15
 @export var muffle_flash: GPUParticles3D
 @export var sparks: PackedScene
+@export var is_automatic: bool
 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var weapon_pos: Vector3 = weapon_mesh.position
 @onready var ray_cast: RayCast3D = $RayCast3D
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("fire"):
-		if cooldown_timer.is_stopped():
-			shoot()
+	if is_automatic:
+		if Input.is_action_pressed("fire"):
+			if cooldown_timer.is_stopped():
+				shoot()
+	else:
+		if Input.is_action_just_pressed("fire"):
+			if cooldown_timer.is_stopped():
+				shoot()
 	
 	weapon_mesh.position = weapon_mesh.position.lerp(weapon_pos, delta * 10.0)
 
