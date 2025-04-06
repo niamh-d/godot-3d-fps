9667cb87786a97be870c64d33ce5317c03dcfbb0
Niamh <niamh.linguist@gmail.com>
2025-04-06 21:51:40 +0300

    pickup variation and fresnel aura

--- Pickups/pickup.gd
+++ Pickups/pickup.gd
@@ -3,6 +3,10 @@ extends Area3D
 @export var ammo_type: AmmoHandler.ammo_type = AmmoHandler.ammo_type.BULLET
 @export var amount := 20
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	animation_player.play("idle")
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
